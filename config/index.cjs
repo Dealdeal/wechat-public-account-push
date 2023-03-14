f2f31e85a790c97d2ae1574588d6f05f6b99fe90
AaronLeung <67881463+Dealdeal@users.noreply.github.com>
2023-03-14 03:25:34 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -114,7 +114,7 @@ const USER_CONFIG = {
         id: 'o9Hve6sE_MZL9TAPTqL5NwodXlj4',
         // 使用微信测试号：你想对他发送的模板消息的模板ID
         // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-        useTemplateId: 'vWWeqI0JEiplEpiUhtoeSRx2OT99CQJQIfBaIaJRwrM',
+        useTemplateId: 'jHVielJa-7pQNPs9xuDVyCGwZqUKbb6iOfcLVnUFq0s',
         // 所在省份或城市，也可以不填
         province: '',
         // 所在城市或县区
